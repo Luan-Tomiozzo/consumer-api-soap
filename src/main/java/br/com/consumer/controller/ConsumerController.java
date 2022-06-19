66f8b956542f775cc7055269d527b8517794d84f
Luan Pedro Tomiozzo <Luan Pedro Tomiozzo@host.docker.internal>
2022-06-19 19:49:17 -0400

    Implementado endpoints:  - servicos;  - servicos;  - servicos;

--- src/main/java/br/com/consumer/controller/ConsumerController.java
+++ src/main/java/br/com/consumer/controller/ConsumerController.java
@@ -27,4 +27,67 @@ public class ConsumerController {
             throw new RestResponseException(ex);
         }
     }
+
+    @GetMapping(path = "/servico-valor-declarado", produces = {"application/json"})
+    public ResponseEntity getServicosValorDeclarado(@RequestParam(name = "usuario", required = false) String usuario, @RequestParam(name = "senha", required = false) String senha) throws Exception {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new ConsumerRepository().getServicosValorDeclarado(usuario, senha));
+        } catch (Exception ex) {
+            throw new RestResponseException(ex);
+        }
+    }
+
+    @GetMapping(path = "/data-atual", produces = {"application/json"})
+    public ResponseEntity getDataAtual() throws Exception {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new ConsumerRepository().getDataAtual());
+        } catch (Exception ex) {
+            throw new RestResponseException(ex);
+        }
+    }
+
+    @GetMapping(path = "/servicos-adicionais-ativos", produces = {"application/json"})
+    public ResponseEntity getServicosAdicionaisAtivos(@RequestParam(name = "usuario", required = false) String usuario, @RequestParam(name = "senha", required = false) String senha) throws Exception {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new ConsumerRepository().getServicosAdicionaisAtivos(usuario, senha));
+        } catch (Exception ex) {
+            throw new RestResponseException(ex);
+        }
+    }
+
+    @GetMapping(path = "/calculo-valor-servico", produces = {"application/json"})
+    public ResponseEntity getCalculoValorServico(@RequestParam(name = "codAdministrativo", required = false) String codAdministrativo,
+            @RequestParam(name = "usuario", required = false) String usuario,
+            @RequestParam(name = "senha", required = false) String senha,
+            @RequestParam(name = "codServico", required = false) String codServico,
+            @RequestParam(name = "cepOrigem", required = false) String cepOrigem,
+            @RequestParam(name = "cepDestino", required = false) String cepDestino,
+            @RequestParam(name = "peso", required = false) String peso,
+            @RequestParam(name = "codFormato", required = false) Integer codFormato,
+            @RequestParam(name = "comprimento", required = false) Double comprimento,
+            @RequestParam(name = "altura", required = false) Double altura,
+            @RequestParam(name = "largura", required = false) Double largura,
+            @RequestParam(name = "diametro", required = false) Double diametro,
+            @RequestParam(name = "codMaoPropria", required = false) String codMaoPropria,
+            @RequestParam(name = "valorDeclarado", required = false) Double valorDeclarado,
+            @RequestParam(name = "codAvisoRecebimento", required = false) String codAvisoRecebimento) throws Exception {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new ConsumerRepository().getCalculoValorServico(codAdministrativo, usuario, senha, codServico, cepOrigem, cepDestino, peso, codFormato, comprimento, altura, largura, diametro, codMaoPropria, valorDeclarado, codAvisoRecebimento));
+        } catch (Exception ex) {
+            throw new RestResponseException(ex);
+        }
+    }
+
+    @GetMapping(path = "/servicos", produces = {"application/json"})
+    public ResponseEntity getServicos(
+            @RequestParam(name = "idContrato", required = false) String idContrato,
+            @RequestParam(name = "idCartaoPostagem", required = false) String idCartaoPostagem,
+            @RequestParam(name = "usuario", required = false) String usuario,
+            @RequestParam(name = "senha", required = false) String senha) throws Exception {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(new ConsumerRepository().getServicos(idContrato, idCartaoPostagem, usuario, senha));
+        } catch (Exception ex) {
+            throw new RestResponseException(ex);
+        }
+    }
 }
--- src/main/java/br/com/consumer/repository/ConsumerRepository.java
+++ src/main/java/br/com/consumer/repository/ConsumerRepository.java
@@ -1,7 +1,14 @@
 package br.com.consumer.repository;
 
 import br.com.correios.bsb.sigep.master.bean.cliente.AtendeClienteService;
+import br.com.correios.bsb.sigep.master.bean.cliente.AutenticacaoException;
 import br.com.correios.bsb.sigep.master.bean.cliente.EnderecoERP;
+import br.com.correios.bsb.sigep.master.bean.cliente.SQLException_Exception;
+import br.com.correios.bsb.sigep.master.bean.cliente.ServicoAdicionalXML;
+import br.com.correios.bsb.sigep.master.bean.cliente.ServicoERP;
+import br.com.correios.bsb.sigep.master.bean.cliente.SigepClienteException;
+import java.util.List;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 /**
  *
@@ -11,5 +18,25 @@ public class ConsumerRepository {
 
     public EnderecoERP getCep(String cep) throws Exception {
         return new AtendeClienteService().getAtendeClientePort().consultaCEP(cep);
+    }
+
+    public List<String> getServicosValorDeclarado(String usuario, String senha) throws Exception {
+        return new AtendeClienteService().getAtendeClientePort().buscaServicosValorDeclarado(usuario, senha);
+    }
+
+    public XMLGregorianCalendar getDataAtual() {
+        return new AtendeClienteService().getAtendeClientePort().buscaDataAtual();
+    }
+
+    public List<ServicoAdicionalXML> getServicosAdicionaisAtivos(String usuario, String senha) throws SQLException_Exception, SigepClienteException {
+        return new AtendeClienteService().getAtendeClientePort().buscaServicosAdicionaisAtivos(usuario, senha);
+    }
+
+    public String getCalculoValorServico(String codAdministrativo, String usuario, String senha, String codServico, String cepOrigem, String cepDestino, String peso, Integer codFormato, Double comprimento, Double altura, Double largura, Double diametro, String codMaoPropria, Double valorDeclarado, String codAvisoRecebimento) throws Exception {
+        return new AtendeClienteService().getAtendeClientePort().calculaTarifaServico(codAdministrativo, usuario, senha, codServico, cepOrigem, cepDestino, peso, codFormato, comprimento, altura, largura, diametro, codMaoPropria, valorDeclarado, codAvisoRecebimento);
+    }
+
+    public List<ServicoERP> getServicos(String idContrato, String idCartaoPostagem, String usuario, String senha) throws AutenticacaoException, SigepClienteException {
+        return new AtendeClienteService().getAtendeClientePort().buscaServicos(idContrato, idCartaoPostagem, usuario, senha);
     }
 }
